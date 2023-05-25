de87a1cf3ac04dfe95d0a3376336af7c985591c4
Laura Connor <itslauraroseco@gmail.com>
2023-05-25 14:19:54 -0400

    fixed button spacing on dashboard

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -11,8 +11,8 @@ export default function Dashboard(){
     return(
         <div className='dashboard-pg flex flex-col justify-between text-center'>
             <img src={ergWelcome} alt="Two people on ergs next to water" />
-            <h1 className='text-2xl mt-6 text-green text-center md:text-5xl md:mt-10'>Welcome {userName}</h1>
-            <div className='flex flex-row justify-around align-center text-center '>
+            <h1 className='text-2xl my-6 text-green text-center md:text-5xl md:mt-10'>Welcome {userName}</h1>
+            <div className='flex justify-center gap-8'>
                 {/* self-center on buttons keeps them from expanding the whole width of page */}
                 <Link to="/addworkout" className='btn coral self-center'>Add Workout</Link>
                 <Link to="/log" className='btn self-center'>View Log</Link>
