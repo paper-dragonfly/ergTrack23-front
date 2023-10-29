0d5da9ee37fe4e60f69518ddc738d85ac94984a1
ncernek <ncernek@gmail.com>
2023-10-29 18:35:06 -0400

    use the firebase emulator

--- src/utils/firebase.tsx
+++ src/utils/firebase.tsx
@@ -1,18 +1,27 @@
 // Import the functions you need from the SDKs 
 import { initializeApp } from "firebase/app";
-import {getAuth, GoogleAuthProvider, signInWithPopup, signOut } from 'firebase/auth' 
-import 'firebase/auth'
+import 'firebase/auth';
+import { Auth, GoogleAuthProvider, connectAuthEmulator, getAuth, signInWithPopup, signOut } from 'firebase/auth';
 import { firebaseConfig } from "../config";
 
-
-// Initialize Firebase
+let auth: Auth;
 const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app);
+
+
+// Check if running on localhost and use the emulator
+if (window.location.hostname === "localhost") {
+    console.log("Using the local emulator for Firebase Auth");
+    auth = getAuth();
+    connectAuthEmulator(auth, "http://127.0.0.1:9099");
+} else {
+    auth = getAuth(app);
+}
 
 //gmail
 export const provider = new GoogleAuthProvider()
 
-export const signInWithGoogle = () => signInWithPopup(auth, provider) 
+export const signInWithGoogle = () => signInWithPopup(auth, provider)
 
-export const firebaseSignOut =  () => signOut(auth)
+export const firebaseSignOut = () => signOut(auth)
 
+export { auth };
