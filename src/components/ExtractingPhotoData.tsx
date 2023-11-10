cd1cf455fedf5902e48ffb5a6a89ffd95198935e
paper-dragonfly <kathleengracenoble@gmail.com>
2023-11-09 20:57:24 -0500

    changed name of Loadingcomponent to ExtractingPhotoData

---
+++ src/components/ExtractingPhotoData.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Alert, Space, Spin } from 'antd';
+
+export default function ExtractingPhotoData () {
+  return(
+    <div className='flex flex-col items-center'>
+  <Space direction="vertical" className='flex flex-col items-center'>
+    <Space>   
+      <Spin  size="large"  >
+        <div className="content " />
+      </Spin>
+    </Space>
+
+    {/* <Spin tip="ExtractingPhotoData..."> */}
+      <Alert
+        message="Extracting your data"
+        description="This can take a minute"
+        type="info"
+        className='my-4'
+      />
+    {/* </Spin> */}
+  </Space>
+  </div>
+  )
+}
